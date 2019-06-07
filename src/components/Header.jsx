46827dd6a598c8079df97262f9bbd90f1ab333fb
孙路 <>
2019-06-07 11:35:27 +0800

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,116 +1,92 @@
 import React from 'react';
-import {
-    IS_PAJK,
-    IS_HRX,
-    IS_FROM_IFRAME,
-    IS_SHOUXIAN,
-    IS_HCZ,
-    IS_FROM_TAB,
-    IS_JSB,
-    IS_SHOW_HEADER,
-    APP_ENV,
-    IS_CBW
-} from '../core/env';
-import { $ } from '../core/dom';
-import * as appsdk from '../core/appsdk';
-import { getActivedPage } from '../core/app';
-import { transformTFSImageUrl } from '../utils/tfs';
-import { createSpmProps } from '../core/log';
+import { $ } from '../utils';
+import * as appsdk from '../app/appsdk';
+import { getActivedPage } from '../app';
 
-var goBack;
-
-if (IS_SHOUXIAN || IS_HCZ || IS_HRX || IS_CBW) {
-    var closeWebViewTimer;
-    goBack = () => {
-        if (closeWebViewTimer) clearTimeout(closeWebViewTimer);
-        closeWebViewTimer = setTimeout(() => {
-            try {
-                getActivedPage().activity.destroy();
-            } catch (e) {
-                console.error(e);
-            }
-            appsdk.exitWebView();
-        }, 600);
-        history.back();
-    };
-    $(window).on('hashchange popstate unload', () => {
-        if (closeWebViewTimer) {
-            clearTimeout(closeWebViewTimer);
-            closeWebViewTimer = null;
+let closeWebViewTimer;
+function goBack() {
+    if (closeWebViewTimer) clearTimeout(closeWebViewTimer);
+    closeWebViewTimer = setTimeout(() => {
+        try {
+            getActivedPage().activity.destroy();
+        } catch (e) {
+            console.error(e);
         }
-    });
-} else {
-    goBack = () => history.back();
-}
+        appsdk.exitWebView();
+    }, 600);
+    history.back();
+};
+
+$(window).on('hashchange popstate unload', () => {
+    if (closeWebViewTimer) {
+        clearTimeout(closeWebViewTimer);
+        closeWebViewTimer = null;
+    }
+});
 
 export default function Header({
     title,
-    bgColor,
-    bgSrc,
+    backgroundColor,
+    backgroundImage,
     color,
     back,
     buttons,
     backText,
     className,
     children,
-    forceShow = false
+    visible = true
 }) {
     if (typeof back == 'string' && !backText) {
         backText = back;
     } else if (back === undefined && process.env.NODE_ENV != "SNAPSHOT") {
-        back = !IS_PAJK && !IS_JSB;
+        back = true;
     }
 
-    const backSpm = process.env.NODE_ENV === "SNAPSHOT" ? {} : createSpmProps('return');
-
-    var style = bgColor ? { backgroundColor: bgColor, borderTopColor: bgColor } : {};
+    const style = backgroundColor ? { backgroundColor, borderTopColor: backgroundColor } : {};
     if (color) {
         style.color = color;
     }
 
-    if (bgSrc) {
-        style.backgroundImage = `url(${transformTFSImageUrl(bgSrc)})`;
+    if (backgroundImage) {
+        style.backgroundImage = `url(${backgroundImage})`;
         style.backgroundRepeat = 'no-repeat';
     }
 
     return (
-        (IS_FROM_IFRAME || !IS_SHOW_HEADER) && !forceShow
+        !visible
             ? null
             : (
                 <header
                     style={style}
-                    className={"header " + (className || '') + (bgColor ? " header_hasbg" : '')}
+                    className={"app-header " + (className || '') + (backgroundColor ? " app-header-with-bg" : '')}
                 >
-                    <div className="header_left">
+                    <div className="app-header-left">
                         {
-                            back && !IS_FROM_TAB &&
+                            back &&
                             <div
-                                className="header_back"
+                                className="app-header-back"
                                 onClick={
                                     typeof back == 'function'
                                         ? back
                                         : goBack
                                 }
-                                app-track="return_click"
-                                app-track-params={JSON.stringify({ channel: APP_ENV })}
-                                {...backSpm}
                             >
                                 <button className="iconfont icon-back fs_22 bg_0000 h_full pl_s pr_s"></button>
                                 {backText || ''}
                             </div>
                         }
                     </div>
-                    <div className="header_con to_e">
+                    <div className="app-header-con to_e">
                         {
                             title
-                                ? <div className="header_title to_e">{title}</div>
+                                ? <div className="app-header-title to_e">{title}</div>
                                 : null
                         }
                         {
                             children
                         }
                     </div>
-                    <div className="header_right">
+                    <div className="app-header-right">
                         {buttons}
                     </div>
                 </header>
