56017e29cb79a105d78b75025afbccfbeba9dd8f
孙路 <>
2019-08-12 10:12:35 +0800

    use preact

--- template/config/webpack.config.js
+++ template/config/webpack.config.js
@@ -275,6 +275,8 @@ module.exports = function (webpackEnv) {
         .map(ext => `.${ext}`)
         .filter(ext => useTypeScript || !ext.includes('ts')),
       alias: {
+        'react': 'preact/compat',
+        'react-dom': 'preact/compat',
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
