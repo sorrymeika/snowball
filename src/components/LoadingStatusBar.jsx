8c573907853429bf1b1862dec6d0478d5f028e81
孙路 <>
2019-11-26 11:30:33 +0800

    优化LoadingStatusBar

--- src/components/LoadingStatusBar.jsx
+++ src/components/LoadingStatusBar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function LoadingStatusBar({ message }) {
+export default function LoadingStatusBar({ message, children, className, ...props }) {
     return (
-        <div className="app-load-more flex">
+        <div className={"app-load-more flex " + (className || '')} {...props}>
             <div className="bd_b fx_1"></div>
-            <p>{message}</p>
+            <p>{message}{children}</p>
             <div className="bd_b fx_1"></div>
         </div>
     );
