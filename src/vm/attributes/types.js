7d0100781445a941f2da0bbf716af384c874d903
孙路 <>
2020-01-13 10:02:18 +0800

    fix observable.any

--- src/vm/attributes/types.js
+++ src/vm/attributes/types.js
@@ -7,7 +7,7 @@ export const any = createDescriptor(
         return (prop && prop.state.facade) || observer.get(name);
     },
     (observer, name, val) => {
-        observer.set(name, val);
+        observer.set(true, name, val);
     }
 );
 
