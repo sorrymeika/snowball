b92b09a23c8d21c3daf197ee9d2de57d2defb246
孙路 <>
2020-01-19 18:40:14 +0800

    优化emitter

--- src/app/controller/emitter.js
+++ src/app/controller/emitter.js
@@ -1,34 +1,44 @@
+import { isString } from "../../utils";
 
-function createEmitterDecorator(type) {
-    return (target, key, { value: fn, configurable, enumerable, initializer }) => {
-        if ((fn !== undefined && typeof fn !== 'function') || initializer) {
-            throw new SyntaxError(`@emitter can only be used on functions!`);
-        }
+function decorate(target, key, { value: fn, configurable, enumerable, initializer }, type) {
+    if ((fn !== undefined && typeof fn !== 'function') || initializer) {
+        throw new SyntaxError(`@emitter can only be used on functions!`);
+    }
 
-        return {
-            configurable: true,
-            enumerable,
+    return {
+        configurable: true,
+        enumerable,
 
-            get() {
-                if (this === target) {
-                    return fn;
-                }
-
-                const boundFn = fn ? fn.bind(this) : undefined;
-                const emitterFn = this.ctx[type === 'asyncEmitter' ? 'createAsyncEmitter' : 'createEmitter'](boundFn);
-
-                Object.defineProperty(this, key, {
-                    configurable: true,
-                    writable: true,
-                    enumerable,
-                    value: emitterFn
-                });
-
-                return emitterFn;
+        get() {
+            if (this === target) {
+                return fn;
             }
-        };
+
+            const boundFn = fn ? fn.bind(this) : undefined;
+            const emitterFn = this.ctx[type === 'async' ? 'createAsyncEmitter' : 'createEmitter'](boundFn);
+
+            Object.defineProperty(this, key, {
+                configurable: true,
+                writable: true,
+                enumerable: true,
+                value: emitterFn
+            });
+
+            return emitterFn;
+        }
     };
 }
 
-export const emitter = createEmitterDecorator('emitter');
-export const asyncEmitter = createEmitterDecorator("asyncEmitter");
+function createEmitterDecorator(type) {
+    return (target, key, descriptor) => {
+        return isString(target)
+            ? (_target, _key, _descriptor) => {
+                Object.defineProperty(_target, target, decorate(_target, target, _descriptor, type));
+                return _descriptor;
+            }
+            : decorate(target, key, descriptor, type);
+    };
+}
+
+export const emitter = createEmitterDecorator();
+export const asyncEmitter = createEmitterDecorator("async");
