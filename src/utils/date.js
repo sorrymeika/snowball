79a2333178ab9ab57f08b2865d617a03c3f58899
孙路 <>
2021-03-16 09:24:08 +0800

    fix date

--- src/utils/date.js
+++ src/utils/date.js
@@ -55,7 +55,7 @@ export function parseDate(date) {
         return parseInt(time, 10);
     });
 
-    return new Date(date[0], date[1] - 1, date[2], date[3], date[4], date[5]);
+    return new Date(date[0], date[1] - 1, date[2], date[3] || 0, date[4] || 0, date[5] || 0);
 }
 
 /**
